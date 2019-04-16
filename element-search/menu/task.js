d1f26811aeb73fd20368ed11c9a25b63f3a30e40
sheerazspb <sheerazspb@gmail.com>
2019-04-16 09:29:29 +0300

    updated  element-search/menu/task.js

---
+++ element-search/menu/task.js
@@ -0,0 +1,17 @@
+let links = document.querySelectorAll('.menu__link');
+
+for (let link of Array.from(links)) {
+    link.onclick = function () {
+
+        let parent = link.parentElement;
+        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+        } else {
+            parent.querySelector('.menu_sub').className = 'menu menu_sub'
+        }
+
+        if (link.closest('.menu_main')) {
+            return false
+        }
+    }
+}
